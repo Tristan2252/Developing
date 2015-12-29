e405ce7c816da30e47118c94cf61ff4882acc744
Tristan2252 <tvigil25@live.com>
2015-12-29 10:46:52 -0700

    basic function done, needs bug fixes

--- ls_parce/ls_parce.c
+++ ls_parce/ls_parce.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #define LEN 1023
-//#define DEBUG 
+#define DEBUG 
 
 char *parce(char c, int off_start, char *string);
 char *parce_range(char c, int off_start, int off_end, char *string);
@@ -15,70 +15,56 @@ int main(void)
 	char list[LEN][LEN];
 	char buf[LEN];
 	
+	printf("\n\t>>> INPUT <<<\n");
 	int line = 0;
 	while (fgets(buf, LEN, stdin)){
 		if((buf[0] == 'd') | (buf[0] == '-')){
 			strncpy(list[line], buf, LEN);
-			//printf("%s", list[line]);
+			printf("%s", list[line]);
 			line++;
 		}
 	}
-/*	
-#ifdef DEBUG
-	int i;
-	char *perm_flags;
-	char *link_num;
-	char *owner;
-	printf("Permissions: Links: Owner:\n");
-	for (i = 0; i < line; i++){
-		perm_flags = strtok(list[i], " ");
-		link_num = strtok(NULL, " ");
-		owner = strtok(NULL, " ");
-		printf("  %s  %s  %s\n", perm_flags, link_num, owner);
-	}
-#endif
-
 	puts("");
 
-//	parce(' ', 4, list[1]);
-//
-*/
-//	printf("%s\n", parce_range(' ', 5, 7, list[4]));
-	printf("%s\n", parce_range('t', 1, 3, list[4]));
+#ifdef DEBUG
+	int i;
+	printf("Permissions: Links: Owner:\n");
+	for (i = 0; i < line; i++){
+		printf("%s\n", parce_range(' ', 0, 2, list[i]));
+	}
+#endif
 
 	return 0;
 }
 
+/**
+ * Function gets the characters between a range of iterations of a character, 
+ * for example if the user wanted all the character between the 1st and 3rd iteration of 't'
+ * @param c the character to search for
+ * @param off_start the offset starting point or the iteration at with to start the range
+ * @param off_end the end of the range or the iteration of c at witch to stop
+ * @param string the string to search
+ * @return new_str the string within the range
+ */
 char *parce_range(char c, int off_start, int off_end, char *string){
 	char *new_str = malloc(LEN * sizeof(char));
-	int cnt = 0; 
-	int cnt1 = 0;
+	int cnt = 0;  // keeps track of c iterations
+	int index = 0; // keeps track of indexes of new string
 	int i;
 	for (i = 0; i < strnlen(string, LEN); i++){
 		if (string[i] == c)
 			cnt++;
 		if (cnt >= off_start && cnt <= off_end){
-			new_str[cnt1] = string[i];
-			cnt1++;
+			if (index == 0 && string[i] == c) // fixes adding first character that is = c
+				;
+			else{
+				new_str[index] = string[i]; // adding characters to new string
+				index++;
+
+			}
 		}
 	}
 
 	return new_str;
 }
 
-/*
-char *parce(char c, int off_start, char *string){
-	int cnt = 0;
-	int i;
-	for (i = 0; i < strnlen(string, LEN); i++){
-		if (string[i] == c && cnt == off_start)
-			;
-		else if (string[i] == c){
-			printf("%s", &string[i]);
-			cnt++;
-		}
-	}
-
-	return string;
-}
-*/

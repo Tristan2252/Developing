33e66db30cd1a7685210dce24070705f044f159f
Tristan2252 <tvigil25@live.com>
2016-01-08 16:01:21 -0700

    ls_time.sh finished

--- ls_parce/comp.c
+++ ls_parce/comp.c
@@ -3,39 +3,64 @@
 #include <unistd.h>
 #include <string.h>
 
-int comp_date(int n1, int n2);
+struct stats_t {
+	int date1;
+	int date2;
+	char time1[1023];
+	char time2[1023];
+	int hour1;
+	int hour2;
+	int min1;
+	int min2;
+};
+
+void swap(int *num1, int *num2);
+int comp_num(int n1, int n2);
 
 int main (int argc, char *argv[])
 {
 	char cmd;
 	char input[1023] = "";
-	int num1;
-	int num2;
+	struct stats_t stats;
 
 	while (cmd != -1){
 		cmd = getopt(argc, argv, "d:t:a:");
 		switch(cmd){
 		case 'd':
 			strncpy(input, optarg, 1023);
-			num1 = atoi(strtok(input, ","));
-			num2 = atoi(strtok(NULL, "\0"));
-			printf("%d", comp_date(num1, num2));
+			stats.date1 = atoi(strtok(input, ","));
+			stats.date2 = atoi(strtok(NULL, "\0"));
+			printf("%d days", comp_num(stats.date1, stats.date2));
 			break;
 		case 't':
-			break;
-		case 'a':
+			strncpy(input, optarg, 1023);
+			strncpy(stats.time1, strtok(input, ","), 1023);
+			strncpy(stats.time2, strtok(NULL, ","), 1023);
+			
+			stats.hour1 = atoi(strtok(stats.time1, ":"));
+			stats.min1 = atoi(strtok(NULL, "\0"));
+			stats.hour2 = atoi(strtok(stats.time2, ":"));
+			stats.min2 = atoi(strtok(NULL, "\0"));
+
+			printf("%d hours and %d minutes", comp_num(stats.hour1, stats.hour2), comp_num(stats.min1, stats.min2));
 			break;
 		}
 	}
 	return 0;
 }
 
-int comp_date(int n1, int n2)
+int comp_num(int n1, int n2)
 {
-	if (n1 < n2){
-		int tmp = n1;
-		n1 = n2;
-		n2 = tmp;
-	}
+	if (n1 < n2)
+		swap(&n1, &n2);
+
 	return n1 - n2;
+}
+
+void swap(int *num1, int *num2)
+{	
+	int tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
+
 }
--- ls_parce/ls_time.sh
+++ ls_parce/ls_time.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 file=$1
 date_out=$(./slice_parce -s 0 -e 1 -i "$(date)" -d :) # gettig rid of the seconds
 
@@ -15,12 +14,15 @@ fi
 file_date=$(./slice_parce -s 7 -e 7 -i "$ls_out")
 sys_date=$(./slice_parce -s 3 -e 3 -i "$date_out")
 
+file_time=$(./slice_parce -s 8 -e 8 -i "$ls_out")
+sys_time=$(./slice_parce -s 4 -e 4 -i "$date_out")
 
+time_dif="$(./comp -d $sys_date,$file_date) $(./comp -t $sys_time,$file_time)"
 
 echo ""
 echo "File date: $(./slice_parce -s 5 -e 8 -i "$ls_out")"
 echo "System date: $(./slice_parce -s 1 -e 4 -i "$date_out")"
-echo "File '$file' is $(./comp -d $sys_date,$file_date) days older then current system time"
+echo "File '$file' is $time_dif older then current system time"
 echo ""
 
 exit 0
